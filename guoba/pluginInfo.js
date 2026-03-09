32fe548fbeb4b48c0b3d2500e70a6aa73dda394d
Temmie0125 <1179755948@qq.com>
2026-03-09 22:19:40 +0800

    fix guoba schemas

--- guoba/pluginInfo.js
+++ guoba/pluginInfo.js
@@ -1,8 +1,20 @@
+/*
+ * @Author: Temmie0125 1179755948@qq.com
+ * @Date: 2026-03-09 21:58:47
+ * @LastEditors: Temmie0125 1179755948@qq.com
+ * @LastEditTime: 2026-03-09 22:15:57
+ * @FilePath: \实验与作业e:\bot\plugins\schedule\guoba\pluginInfo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export default {
     name: "schedule-plugin",
     title: "课程表插件",
     description: "WakeUp课程表导入与查询，支持课表订阅推送",
-    author: "Temmie0125",
+    author: "Temmie",
+    authorLink: 'https://github.com/Temmie0125',
+    link: 'https://github.com/Temmie0125/Yunzai-Schedule-Plugin',
+    isV3: true,
+    isV2: false,
     version: "1.0.0",
     icon: "mdi:calendar-clock",
     depends: []
