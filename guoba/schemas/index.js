6537f074b8c7d17f13671efee95a647da1472cbd
Temmie0125 <1179755948@qq.com>
2026-03-10 00:31:38 +0800

    remove 咕咕咕(?

--- guoba/schemas/index.js
+++ guoba/schemas/index.js
@@ -14,9 +14,5 @@ export const schemas = [
         required: true,
         placeholder: "例如：0 20 * * * 表示每天20:00",
         bottomHelpMessage: "请填写标准的cron表达式，支持Linux crontab格式。修改后需要重启 Bot 才能生效"
-    },
-    {
-        label: '咕咕中',
-        component: 'Divider'
     }
 ]
